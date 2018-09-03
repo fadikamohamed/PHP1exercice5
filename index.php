5d0a51a3c2af9814a1004792fef21515c2aa6491
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:43:08 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $intVar = NULL;
+  echo $intVar;
+  $intVar = 42;
+  echo $intVar;
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
